4d3f1f8e280db079ee3cb88f5b7a193a1d8f53ab
Simon CHEREL <simon.cherel@gmail.com>
2022-12-28 18:37:21 +0100

    Deploy portfolio on remote gh-pages by the branch deploy

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@
 //   reactStrictMode: true,
 //   swcMinify: true,
 // }
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig   = {
-  assetPrefix: isProd ? '/simon-cherel/' : './',
+  assetPrefix: './',
   images: {
     unoptimized: true,
   },
